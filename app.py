c627df750fcd1f15ce28eda5c094bbc0185b98b6
Steven Estremadoyro Luque <sagitario1213@hotmail.com>
2022-10-17 03:41:33 -0500

    mahial

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ def plan_execute():
     if model['working'] == False:
         import subprocess
         #import sys
-        cmd='nohup python -u /home/semaforo/api-raspberry/task.py > /home/semaforo/api-raspberry/cron.log &'
+        cmd='nohup python -u /home/semaforo/api-raspberry/task.py > /home/semaforo/api-raspberry/manual.log &'
         subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #o, e = proc.communicate()
         #print('Error: '  + e.decode('ascii'))
